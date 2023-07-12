650943718c82b24087788e7d7469282cd0639c9d
Johnny Ho <jh@berkeley.edu>
2023-07-12 10:39:42 -0400

    add efficiencies of Amptek SDD and SRI CMOS

--- src/xrcf/utilities.py
+++ src/xrcf/utilities.py
@@ -6,7 +6,9 @@ from scipy.interpolate import interp1d
 
 logger = logging.getLogger('xrcf')
 
+amptek_dir = '../src/xrcf/data/amptek/'
 henke_dir = '../src/xrcf/data/henke/'
+sri_dir = '../src/xrcf/data/sri/'
 gasses = [ 'p10' ]
 pressures = [ 400, 600, 800 ]
 
@@ -35,6 +37,51 @@ def electronvolts(channel, α, β):
 
     """
     return (channel - α) / β
+
+#//////////////////////////////////////////////////////////////////////////////
+# efficiency of Amptek SDD with C1/C2 window
+#//////////////////////////////////////////////////////////////////////////////
+def sdd_c1_c2_efficiency_function(window='c1', efficiency='pe'):
+
+    # check if Amptek data file exists
+    file_path = amptek_dir + 'c1_c2_efficiency.txt'
+    if not os.path.isfile(file_path):
+        raise IOError(errno.ENOENT, 'No such file or directory', file_path)
+
+    data = np.loadtxt(file_path, skiprows=12, dtype=float)
+
+    x = data[:, 0] * 1e3
+    y = data[:, 2]
+
+    if window == 'c1' and efficiency == 'total':
+        y = data[:, 1]
+    elif window == 'c1' and efficiency == 'pe':
+        y = data[:, 2]
+    elif window == 'c2' and efficiency == 'total':
+        y = data[:, 3]
+    elif window == 'c2' and efficiency == 'pe':
+        y = data[:, 4]
+    else:
+        raise ValueError('Invalid (window, efficiency) values: ({}, {}); valid window values: c1 or c2; valid efficiency values: total or pe'.format(window, efficiency))
+
+    return interp1d(x, y)
+
+#//////////////////////////////////////////////////////////////////////////////
+# efficiency of SRI CMOS imager
+#//////////////////////////////////////////////////////////////////////////////
+def sri_cmos_efficiency_function():
+
+    # check if SRI data file exists
+    file_path = sri_dir + 'EIS_no_ARC_CMOS.dat'
+    if not os.path.isfile(file_path):
+        raise IOError(errno.ENOENT, 'No such file or directory', file_path)
+
+    data = np.loadtxt(file_path, skiprows=0, dtype=float)
+
+    x = data[:, 0]
+    y = data[:, 1]
+
+    return interp1d(x, y)
 
 #//////////////////////////////////////////////////////////////////////////////
 # transmission of VYNS window
